7a179849a5efe8b37c084c30c8ba93b172559328
WXX <wxx_4719@qq.com>
2026-01-30 17:22:27 +0800

    submit all

--- tests/test_demo_multi_environment.py
+++ tests/test_demo_multi_environment.py
@@ -19,8 +19,11 @@ class TestPytestMultiEnvDemo:
         post_api = env_config["postAPI"]
         post_api_request_data = env_request_data["postAPI"]
         post_api_response_data = env_response_data["postAPI"]
-        # send request
-        response = requests.post(host + post_api, post_api_request_data)
-        # assert
+
+        response = requests.post(host + post_api, json=post_api_request_data)
+        resp_json = response.json()
+
         assert response.status_code == 201
-        assert response.json() == post_api_response_data
+        assert resp_json["title"] == post_api_response_data["title"]
+        assert resp_json["body"] == post_api_response_data["body"]
+        assert int(resp_json["userId"]) == post_api_response_data["userId"]
