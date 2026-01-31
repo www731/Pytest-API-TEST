77ba671a04fd76e8040fe466247a3ef860e8a37f
WXX <wxx_4719@qq.com>
2026-01-31 14:14:57 +0800

    Integrate Allure reports

---
+++ tests/test_demo_allure.py
@@ -0,0 +1,40 @@
+import allure
+import requests
+
+
+@allure.feature("Test example API")
+class TestPytestAllureDemo:
+
+    @allure.story("Test example get endpoint")
+    @allure.title("Verify the get API")
+    @allure.description("verify the get API response status code and data")
+    @allure.severity("blocker")
+    def test_get_example_endpoint_allure(self, env_config, env_request_data, env_response_data):
+        host = env_config["host"]
+        get_api = env_config["getAPI"]
+        get_api_request_data = env_request_data["getAPI"]
+        get_api_response_data = env_response_data["getAPI"]
+        # send get request
+        response = requests.get(host + get_api)
+        # assert
+        print("response status code is" + str(response.status_code))
+        assert response.status_code == 200
+        print("response data is" + str(response.json()))
+        assert response.json() == get_api_response_data
+
+    @allure.story("Test example POST API")
+    @allure.title("Verify the POST API")
+    @allure.description("verify the POST API response status code and data")
+    @allure.severity("Critical")
+    def test_post_example_endpoint_allure(self, env_config, env_request_data, env_response_data):
+        host = env_config["host"]
+        post_api = env_config["postAPI"]
+        post_api_request_data = env_request_data["postAPI"]
+        post_api_response_data = env_response_data["postAPI"]
+        # send request
+        response = requests.post(host + post_api, json=post_api_request_data)
+        # assert
+        print("response status code is" + str(response.status_code))
+        assert response.status_code == 201
+        print("response data is" + str(response.json()))
+        assert response.json() == post_api_response_data
